f40285639e925473b626c22e3105ae32d739ac5d
Daniel Tilahun <106193338+Danitilahun@users.noreply.github.com>
2024-02-08 12:27:08 +0300

    Update message_type.go

--- peercommunication/message_type.go
+++ peercommunication/message_type.go
@@ -3,14 +3,24 @@ package peercommunication
 type MessageType byte
 
 const (
-	MsgTypeChoke         MessageType = 0
-	MsgTypeUnChoke       MessageType = 1
-	MsgTypeInterested    MessageType = 2
+// Tells a peer to stop sending requests.
+	MsgTypeChoke MessageType = 0
+	// Tells a peer that it can start sending requests.
+	MsgTypeUnChoke MessageType = 1
+	// Indicates interest in receiving data from a peer.
+	MsgTypeInterested MessageType = 2
+	// Indicates no interest in receiving further data from a peer.
 	MsgTypeNotInterested MessageType = 3
-	MsgTypeHave          MessageType = 4
-	MsgTypeBitField      MessageType = 5
-	MsgTypeRequest       MessageType = 6
-	MsgTypePiece         MessageType = 7
-	MsgTypeCancel        MessageType = 8
-	MsgTypeKeepAlive     MessageType = 9
+	// Notifies peers that the sender has received a piece of data.
+	MsgTypeHave MessageType = 4
+	//  Provides a bitfield representing the data pieces a peer has.
+	MsgTypeBitField MessageType = 5
+	// Requests a piece of data from a peer.
+	MsgTypeRequest MessageType = 6
+	// Delivers a piece of data in response to a request.
+	MsgTypePiece MessageType = 7
+	// Cancels a previously made request.
+	MsgTypeCancel MessageType = 8
+	// A keep-alive message, likely to maintain the connection alive in periods of inactivity.
+	MsgTypeKeepAlive MessageType = 9
 )
